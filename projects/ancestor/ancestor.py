77400d55c2ecca1b533a723ca6793901e9e6c079
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-07-15 12:42:16 -0700

    ancestor problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,21 @@
-
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # verify at least one parent of the starting_node or else return -1
+    queue = []
+    parent = None
+    for pair in ancestors:
+        if starting_node == pair[1]:
+            parent = pair
+            break
+    # print(parent)
+    if parent == None:
+        return -1
+    queue.append(parent)
+    while len(queue) > 0:
+        # print(queue)
+        node = queue.pop(0)
+        parent = node[0]
+        for pair in ancestors:
+            if pair[1] == parent:
+                queue.append(pair)
+                break
+    return parent
